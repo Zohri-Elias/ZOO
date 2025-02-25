a3d10f7009f8ce99b4fb5c560fd599ad27aedc3c
unknown <zohrielias95@gmail.com>
2025-02-25 01:53:06 +0100

    gestion de visiteur

---
+++ src/bibliotheque/Visiteur.java
@@ -0,0 +1,27 @@
+package bibliotheque;
+
+public class Visiteur {
+    private String nom;
+    private int age;
+    private boolean billet;
+
+    public Visiteur(String nom, int age) {
+        this.nom = nom;
+        this.age = age;
+        this.billet = false;
+    }
+
+    public void acheterBillet() {
+        this.billet = true;
+        System.out.println(nom + " a acheté un billet.");
+    }
+
+    public void interagir(Animal a) {
+        if (billet) {
+            System.out.println(nom + " dit: Quel bel animal!");
+            a.faireDuBruit();
+        } else {
+            System.out.println(nom + " doit acheter un billet pour interagir.");
+        }
+    }
+}
